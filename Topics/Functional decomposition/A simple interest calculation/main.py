715fd54fc0d2235a93ad949ed48ef24ba03a30ca
Alexey Kucheryavenko <kucheryavenko.alexey@gmail.com>
2026-01-15 21:34:06 +0300

    +

---
+++ Topics/Functional decomposition/A simple interest calculation/main.py
@@ -0,0 +1,11 @@
+from functools import total_ordering
+from html.parser import interesting_normal
+
+
+def calculate(amount, interest_rate, time):
+    interest = (amount * interest_rate * time) / 100
+    total_amount = amount + interest
+    return interest, total_amount
+
+def print_result(interest, total_amount):
+    print(f"The interest is: {interest}\nThe total amount is: {total_amount}")
